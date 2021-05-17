df82c0824f50691e3dbc924b5a547aada44509c0
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2021-05-17 13:31:52 +0300

    Footer layout and media queries

--- client/src/elements/Footer.js
+++ client/src/elements/Footer.js
@@ -6,42 +6,44 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="footer-slogan">
-        <img
-          className="footer-image"
-          src="./assets/logos/hoivapalvelu-anneliina-darkblue.png"
-          alt=""
-        />
-        <p className="bold-text">
-          Luotettavaa ja ammattitaitoista hoitopalvelua Etelä-Suomen alueella.
-        </p>
-        <Button text="Ota yhteyttä" className="footer-button" />
+        <div className="footer-slogan-content">
+          <img
+            className="footer-image"
+            src="./assets/logos/hoivapalvelu-anneliina-darkblue.png"
+            alt=""
+          />
+          <p className="bold-text">
+            Luotettavaa ja ammattitaitoista hoitopalvelua Etelä-Suomen alueella.
+          </p>
+        </div>
+        <Button text="Ota yhteyttä" className="footer-slogan-button" />
       </div>
       <div className="footer-information">
         <div className="footer-information-services">
           <h3 className="bold-text">Palvelut</h3>
           <div className="footer-information-content-items">
-            <p>Anneliinan Kotisairaanhoito</p>
-            <p>Anneliinan Kotiapu</p>
-            <p>Anneliinan Päivätoiminta</p>
+            <p className="text-small">Anneliinan Kotisairaanhoito</p>
+            <p className="text-small">Anneliinan Kotiapu</p>
+            <p className="text-small">Anneliinan Päivätoiminta</p>
           </div>
         </div>
         <div className="footer-information-contact">
           <h3 className="bold-text">Yhteystiedot</h3>
           <div className="footer-information-content-items">
-            <p>Hoivapalvelu Anneliina Oy</p>
-            <p className="text-small">
+            <p className="text-small">Hoivapalvelu Anneliina Oy</p>
+            <p className="text-extrasmall">
               <span className="material-icons-outlined footer-information-contact-icon">
                 location_on
               </span>
               Tupalantie 12, 04400 Järvenpää
             </p>
-            <p className="text-small">
+            <p className="text-extrasmall">
               <span className="material-icons-outlined footer-information-contact-icon">
                 phone
               </span>
               0400 877 966
             </p>
-            <p className="text-small">
+            <p className="text-extrasmall">
               <span className="material-icons-outlined footer-information-contact-icon">
                 email
               </span>
