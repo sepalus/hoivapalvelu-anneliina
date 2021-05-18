4000515e977a0941ac7416efb2d0305ca77e5a4e
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2021-05-18 11:19:29 +0300

    When scrolling in scroll area, check and update position of snap scroll area

--- client/src/components/ScrollArea.js
+++ client/src/components/ScrollArea.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ScrollArea.scss";
 
 export default function ScrollArea({ children }) {
+  const [previousSnapScrollYOffset, setPreviousSnapScrollYOffset] = useState(0);
+  const contentHeight = window.innerHeight - 60;
+
+  useEffect(() => {
+    const scrollElement = document.getElementById("scroll-area");
+    const snapScrollElement = document.getElementById("snap-scroll-area");
+
+    const updateSnapScrollPosition = () => {
+      const currentSnapScrollYOffset = snapScrollElement.scrollTop;
+      if (currentSnapScrollYOffset < contentHeight)
+        snapScrollElement.scrollTop = contentHeight;
+      setPreviousSnapScrollYOffset(currentSnapScrollYOffset);
+    };
+
+    scrollElement.addEventListener("scroll", updateSnapScrollPosition);
+    return () =>
+      scrollElement.removeEventListener("scroll", updateSnapScrollPosition);
+  }, [previousSnapScrollYOffset]);
+
   return (
     <div id="scroll-area" className="scroll-area">
       {children}
--- client/src/components/SnapScrollArea.js
+++ client/src/components/SnapScrollArea.js
@@ -1,35 +1,9 @@
-import React, { useState, useEffect } from "react";
-import classNames from "classnames";
-
+import React from "react";
 import "./SnapScrollArea.scss";
 
 export default function SnapScrollArea({ children }) {
-  const [previousYOffset, setPreviousYOffset] = useState(0);
-  const [freezeChildScroll, setFreezeChildScroll] = useState(true);
-  const contentHeight = window.innerHeight - 60;
-
-  useEffect(() => {
-    const snapScrollElement = document.getElementById("snap-scroll-area");
-
-    const detectViewportOnScroll = () => {
-      const currentYOffset = snapScrollElement.scrollTop;
-      setPreviousYOffset(currentYOffset);
-      setFreezeChildScroll(currentYOffset < contentHeight);
-    };
-
-    snapScrollElement.addEventListener("scroll", detectViewportOnScroll);
-    return () =>
-      snapScrollElement.removeEventListener("scroll", detectViewportOnScroll);
-  }, [previousYOffset, freezeChildScroll]);
-
   return (
-    <div
-      id="snap-scroll-area"
-      className={classNames("snap-scroll-area", {
-        "freeze-child-scroll": freezeChildScroll,
-        "unfreeze-child-scroll": !freezeChildScroll,
-      })}
-    >
+    <div id="snap-scroll-area" className="snap-scroll-area">
       {children}
     </div>
   );
