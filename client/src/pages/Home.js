6fe442f6eab64163b03a0db1de7a0841d4958e0c
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2021-05-19 14:39:45 +0300

    Make abckground colors based on screensize

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -21,7 +21,10 @@ export default function Home() {
         />
         <ScrollArea>
           <Slogan />
-          <InfoCardContainer title="Yksilöllistä hoivapalvelua kaikille sitä tarvitseville">
+          <InfoCardContainer
+            title="Yksilöllistä hoivapalvelua kaikille sitä tarvitseville"
+            backgroundColor="lightblue"
+          >
             <InfoCard
               iconFileName="anneliinan-kotisairaanhoito.png"
               logoFileName="anneliinan-kotisairaanhoito.png"
@@ -42,27 +45,27 @@ export default function Home() {
               title="Jokainen meistä on yksilö"
               text="Jokainen meistä on yksilö. Meillä on kaikilla oma tarinamme, omat kokemuksemme, omat ilomme ja surumme ja ennen kaikkea oma tapa elää. Me Anneliinahoivassa pidämme ihmisiä ihmisinä, emme ikänä tai sairautena. Yksilöllinen sydämestä lähtevä hoivamme tarkoittaa sitä, että asiakasta kohdellaan ainutlaatuisena ihmisenä, jolla on paljon annettavaa ja oikeus hyvään elämään."
               buttonText="Lue lisää arvoistamme"
-              backgroundColor="beige"
+              backgroundColorMedium="beige"
               imagePosition="100% 0"
             />
             <ContentBlock
               title="Voisitko sinä tai läheisesi hyötyä palvelustamme?"
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               buttonText="Jätä yhteydenottopyyntö"
-              backgroundColor="lightblue"
+              backgroundColorMedium="ultralightblue"
             />
             <ContentBlock
               imageFileName="home-trust.jpeg"
               title="Olemme luotettava kumppanisi"
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               buttonImage="luotettava-kumppani.png"
-              backgroundColor="beige"
+              backgroundColorMedium="beige"
             />
             <ContentBlock
               title="Oletko sinä seuraava Anneliinalainen?"
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               buttonText="Ota yhteyttä"
-              backgroundColor="lightblue"
+              backgroundColorMedium="ultralightblue"
             />
           </ContentBlockContainer>
           <Footer />
