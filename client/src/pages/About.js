85adfb9cfe124f4edf613f412fec5934fd55c66f
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2021-05-19 13:01:55 +0300

    Add elements to about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,29 @@
 import React from "react";
+import Banner from "../elements/Banner";
+import InfoCardContainer from "../elements/InfoCardContainer";
+import InfoCard from "../elements/InfoCard";
+import ContentBlockContainer from "../elements/ContentBlockContainer";
+import ContentBlock from "../elements/ContentBlock";
+import Footer from "../elements/Footer";
+
+import ScrollArea from "../components/ScrollArea";
 
 export default function About() {
-  return <div className="about">About</div>;
+  return (
+    <div className="about">
+      <ScrollArea>
+        <Banner />
+        <InfoCardContainer title="Lorem ipsum dolor sit amet">
+          <InfoCard />
+          <InfoCard />
+          <InfoCard />
+          <InfoCard />
+        </InfoCardContainer>
+        <ContentBlockContainer>
+          <ContentBlock />
+        </ContentBlockContainer>
+        <Footer />
+      </ScrollArea>
+    </div>
+  );
 }
