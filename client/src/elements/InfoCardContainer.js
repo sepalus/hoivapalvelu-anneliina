0dcab3bc0686725e4f99130747d71501d56e88f6
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2021-05-20 11:44:23 +0300

    Add scroll event listener that hides button when not needed

--- client/src/elements/InfoCardContainer.js
+++ client/src/elements/InfoCardContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import classNames from "classnames";
 import "./InfoCardContainer.scss";
 import Button from "../components/Button";
@@ -10,6 +10,29 @@ export default function InfoCardContainer({
   className,
   expanderElement,
 }) {
+  const infoCardContainerCardsRef = useRef();
+  const [previousScrollXOffset, setPreviousScrollXOffset] = useState(0);
+  const [showBackButton, setShowBackButton] = useState(true);
+
+  useEffect(() => {
+    if (!infoCardContainerCardsRef.current) return;
+    const updateButtonVisibility = () => {
+      const currentScrollXOffset = infoCardContainerCardsRef.current.scrollLeft;
+      setShowBackButton(currentScrollXOffset > 10);
+      setPreviousScrollXOffset(currentScrollXOffset);
+    };
+
+    infoCardContainerCardsRef.current.addEventListener(
+      "scroll",
+      updateButtonVisibility
+    );
+    return () =>
+      infoCardContainerCardsRef.current.removeEventListener(
+        "scroll",
+        updateButtonVisibility
+      );
+  }, [previousScrollXOffset, infoCardContainerCardsRef]);
+
   return (
     <div
       className={classNames("info-card-container", {
@@ -20,10 +43,19 @@ export default function InfoCardContainer({
       <div className="info-card-container-content-wrapper">
         <h2>{title}</h2>
         <div className="info-card-container-card-wrapper">
-          <Button className="info-card-container-button">
+          <Button
+            className={classNames("info-card-container-button", {
+              "hide-element": !showBackButton,
+            })}
+          >
             <span class="material-icons-outlined">arrow_back_ios</span>
           </Button>
-          <div className="info-card-container-cards">{children}</div>
+          <div
+            ref={infoCardContainerCardsRef}
+            className="info-card-container-cards"
+          >
+            {children}
+          </div>
           <Button className="info-card-container-button">
             <span class="material-icons-outlined">arrow_forward_ios</span>
           </Button>
