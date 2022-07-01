66854f85a3a20ba536895573596de5881f6e4a5e
Sebastian Högnabba <sebastian.hognabba@gmail.com>
2022-07-01 23:58:22 +0300

    Update express server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build//static")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -13,7 +13,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
